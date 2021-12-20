bc2eb2b7285df8762ec2135bfbaeb99db6168a79
MuhammadAnas14 <anasm9877@gmail.com>
2021-12-20 23:26:12 +0500

    video added

--- src/Components/video.js
+++ src/Components/video.js
@@ -1,10 +1,15 @@
 import React from "react";
 import {Container, Button } from "react-bootstrap";
 import "./CSS/video.css";
+import sample from './Assets/Course_Overview.mp4'
+
 
 const Video = () => {
   return (
     <div className="video-screen">
+      <video className='videoTag' autoPlay loop>
+    <source src={sample} type='video/mp4' />
+      </video>
       <Container>
         <div className="wrapper">
           <h3 className="head1">Web cam is started</h3>
